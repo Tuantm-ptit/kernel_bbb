d1395a13b3a9b0d77fd33e8348c16ea705e82bcc
root <root@beaglebone.localdomain>
2023-04-21 13:24:11 +0000

    init

---
+++ linux-headers-4.19.94-ti-r42/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1buster */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1buster SMP PREEMPT Tue Mar 31 19:38:29 UTC 2020"
+#define LINUX_COMPILE_BY "voodoo"
+#define LINUX_COMPILE_HOST "x3-am57xx-beagle-x15-2gb"
+#define LINUX_COMPILER "gcc version 8.3.0 (Debian 8.3.0-6)"
---
+++ linux-headers-4.19.94-ti-r42/scripts/dtc/libfdt/.fdt_overlay.o.cmd
@@ -0,0 +1,60 @@
+cmd_scripts/dtc/libfdt/fdt_overlay.o := gcc -Wp,-MD,scripts/dtc/libfdt/.fdt_overlay.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89 -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64    -Iscripts/dtc/libfdt  -c -o scripts/dtc/libfdt/fdt_overlay.o scripts/dtc/libfdt/fdt_overlay.c
+
+source_scripts/dtc/libfdt/fdt_overlay.o := scripts/dtc/libfdt/fdt_overlay.c
+
+deps_scripts/dtc/libfdt/fdt_overlay.o := \
+  /usr/include/stdc-predef.h \
+  scripts/dtc/libfdt/libfdt_env.h \
+  /usr/lib/gcc/arm-linux-gnueabihf/8/include/stddef.h \
+  /usr/lib/gcc/arm-linux-gnueabihf/8/include/stdint.h \
+  /usr/include/stdint.h \
+  /usr/include/arm-linux-gnueabihf/bits/libc-header-start.h \
+  /usr/include/features.h \
+  /usr/include/arm-linux-gnueabihf/sys/cdefs.h \
+  /usr/include/arm-linux-gnueabihf/bits/wordsize.h \
+  /usr/include/arm-linux-gnueabihf/bits/long-double.h \
+  /usr/include/arm-linux-gnueabihf/gnu/stubs.h \
+  /usr/include/arm-linux-gnueabihf/gnu/stubs-hard.h \
+  /usr/include/arm-linux-gnueabihf/bits/types.h \
+  /usr/include/arm-linux-gnueabihf/bits/typesizes.h \
+  /usr/include/arm-linux-gnueabihf/bits/wchar.h \
+  /usr/include/arm-linux-gnueabihf/bits/stdint-intn.h \
+  /usr/include/arm-linux-gnueabihf/bits/stdint-uintn.h \
+  /usr/include/stdlib.h \
+  /usr/include/arm-linux-gnueabihf/bits/waitflags.h \
+  /usr/include/arm-linux-gnueabihf/bits/waitstatus.h \
+  /usr/include/arm-linux-gnueabihf/bits/floatn.h \
+  /usr/include/arm-linux-gnueabihf/bits/floatn-common.h \
+  /usr/include/arm-linux-gnueabihf/sys/types.h \
+  /usr/include/arm-linux-gnueabihf/bits/types/clock_t.h \
+  /usr/include/arm-linux-gnueabihf/bits/types/clockid_t.h \
+  /usr/include/arm-linux-gnueabihf/bits/types/time_t.h \
+  /usr/include/arm-linux-gnueabihf/bits/types/timer_t.h \
+  /usr/include/endian.h \
+  /usr/include/arm-linux-gnueabihf/bits/endian.h \
+  /usr/include/arm-linux-gnueabihf/bits/byteswap.h \
+  /usr/include/arm-linux-gnueabihf/bits/uintn-identity.h \
+  /usr/include/arm-linux-gnueabihf/sys/select.h \
+  /usr/include/arm-linux-gnueabihf/bits/select.h \
+  /usr/include/arm-linux-gnueabihf/bits/types/sigset_t.h \
+  /usr/include/arm-linux-gnueabihf/bits/types/__sigset_t.h \
+  /usr/include/arm-linux-gnueabihf/bits/types/struct_timeval.h \
+  /usr/include/arm-linux-gnueabihf/bits/types/struct_timespec.h \
+  /usr/include/arm-linux-gnueabihf/bits/pthreadtypes.h \
+  /usr/include/arm-linux-gnueabihf/bits/thread-shared-types.h \
+  /usr/include/arm-linux-gnueabihf/bits/pthreadtypes-arch.h \
+  /usr/include/alloca.h \
+  /usr/include/arm-linux-gnueabihf/bits/stdlib-bsearch.h \
+  /usr/include/arm-linux-gnueabihf/bits/stdlib-float.h \
+  /usr/include/string.h \
+  /usr/include/arm-linux-gnueabihf/bits/types/locale_t.h \
+  /usr/include/arm-linux-gnueabihf/bits/types/__locale_t.h \
+  /usr/include/strings.h \
+  scripts/dtc/libfdt/fdt.h \
+  scripts/dtc/libfdt/libfdt.h \
+  scripts/dtc/libfdt/fdt.h \
+  scripts/dtc/libfdt/libfdt_internal.h \
+
+scripts/dtc/libfdt/fdt_overlay.o: $(deps_scripts/dtc/libfdt/fdt_overlay.o)
+
+$(deps_scripts/dtc/libfdt/fdt_overlay.o):
